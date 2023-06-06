e62fa71b88bb0e1dc52bb2c614c5bb083f29867a
AmmadR <ammadraza27@gmail.com>
2023-06-06 16:51:44 +0500

    Updated Main Menu

--- Assets/Script/Ammad/DummyAppOperator/DummyAppOperator.cs
+++ Assets/Script/Ammad/DummyAppOperator/DummyAppOperator.cs
@@ -1,13 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 using UnityEngine.SceneManagement;
 
 public class DummyAppOperator : MonoBehaviour
 {
-    public void UponReset() 
+    [SerializeField] private float colorChangeSpeed = 1f;
+
+    [Space]
+    [SerializeField] private Image screen;
+    [SerializeField] private Color defaultColor;
+    [SerializeField] private Color endColor;
+
+    private void Awake()
     {
-        SceneManager.LoadScene(0);
+        screen.color = endColor;
+        UponGameStart();
+    }
+
+    public void UponReset()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void OnQuit()
+    {
+        Application.Quit();
+    }
+
+    public void UponGameStart()
+    {
+        StartCoroutine(ManageScreenColor(true));
+    }
+
+    public void UponGameEnd()
+    {
+        StartCoroutine(ManageScreenColor(false));
+    }
+
+    IEnumerator ManageScreenColor(bool isStarting)
+    {
+        Color currentColor = screen.color;
+        float t = 0f;
+
+        while ((isStarting && currentColor != defaultColor) || (!isStarting && currentColor != endColor))
+        {
+            t += Time.deltaTime * colorChangeSpeed;
+            if (isStarting)
+            {
+                currentColor = Color.Lerp(currentColor, defaultColor, t);
+            }
+            else
+            {
+                currentColor = Color.Lerp(currentColor, endColor, t);
+            }
+
+            screen.color = currentColor;
+            yield return null;
+        }
     }
 }
