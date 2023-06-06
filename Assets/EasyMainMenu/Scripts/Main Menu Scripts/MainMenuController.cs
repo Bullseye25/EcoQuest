e33105afbaf0d6803e368e6d3a46ae6a541f2f64
Muhammad Mustakeem <89236528+mustaqimjaffri046@users.noreply.github.com>
2023-06-06 17:57:51 +0500

    MainMenu
    Added a MainMenu

---
+++ Assets/EasyMainMenu/Scripts/Main Menu Scripts/MainMenuController.cs
@@ -0,0 +1,192 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenuController : MonoBehaviour {
+
+    Animator anim;
+
+    public string newGameSceneName;
+    public int quickSaveSlotID;
+
+    [Header("Options Panel")]
+    public GameObject MainOptionsPanel;
+    public GameObject StartGameOptionsPanel;
+    public GameObject GamePanel;
+    public GameObject ControlsPanel;
+    public GameObject GfxPanel;
+    public GameObject LoadGamePanel;
+    [SerializeField] private GameObject pausePanel;
+    [SerializeField] private GameObject player;
+
+    // Use this for initialization
+    void Start () {
+        anim = GetComponent<Animator>();
+
+        //new key
+        PlayerPrefs.SetInt("quickSaveSlot", quickSaveSlotID);
+    }
+
+    #region Open Different panels
+
+    public void openOptions()
+    {
+        //enable respective panel
+        MainOptionsPanel.SetActive(true);
+        StartGameOptionsPanel.SetActive(false);
+
+        //play anim for opening main options panel
+        anim.Play("buttonTweenAnims_on");
+
+        //play click sfx
+        playClickSound();
+
+        //enable BLUR
+        //Camera.main.GetComponent<Animator>().Play("BlurOn");
+       
+    }
+
+    public void openStartGameOptions()
+    {
+        //enable respective panel
+        MainOptionsPanel.SetActive(false);
+        StartGameOptionsPanel.SetActive(true);
+
+        //play anim for opening main options panel
+        anim.Play("buttonTweenAnims_on");
+
+        //play click sfx
+        playClickSound();
+
+        //enable BLUR
+        //Camera.main.GetComponent<Animator>().Play("BlurOn");
+        
+    }
+
+    public void openOptions_Game()
+    {
+        //enable respective panel
+        GamePanel.SetActive(true);
+        ControlsPanel.SetActive(false);
+        GfxPanel.SetActive(false);
+        LoadGamePanel.SetActive(false);
+
+        //play anim for opening game options panel
+        anim.Play("OptTweenAnim_on");
+
+        //play click sfx
+        playClickSound();
+
+    }
+    public void openOptions_Controls()
+    {
+        //enable respective panel
+        GamePanel.SetActive(false);
+        ControlsPanel.SetActive(true);
+        GfxPanel.SetActive(false);
+        LoadGamePanel.SetActive(false);
+
+        //play anim for opening game options panel
+        anim.Play("OptTweenAnim_on");
+
+        //play click sfx
+        playClickSound();
+
+    }
+    public void openOptions_Gfx()
+    {
+        //enable respective panel
+        GamePanel.SetActive(false);
+        ControlsPanel.SetActive(false);
+        GfxPanel.SetActive(true);
+        LoadGamePanel.SetActive(false);
+
+        //play anim for opening game options panel
+        anim.Play("OptTweenAnim_on");
+
+        //play click sfx
+        playClickSound();
+
+    }
+
+    public void openContinue_Load()
+    {
+        //enable respective panel
+        GamePanel.SetActive(false);
+        ControlsPanel.SetActive(false);
+        GfxPanel.SetActive(false);
+        LoadGamePanel.SetActive(true);
+
+        //play anim for opening game options panel
+        anim.Play("OptTweenAnim_on");
+
+        //play click sfx
+        playClickSound();
+
+    }
+
+    public void newGame()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+    #endregion
+
+    #region Back Buttons
+
+    public void back_options()
+    {
+        //simply play anim for CLOSING main options panel
+        anim.Play("buttonTweenAnims_off");
+
+        //disable BLUR
+       // Camera.main.GetComponent<Animator>().Play("BlurOff");
+
+        //play click sfx
+        playClickSound();
+    }
+
+    public void back_options_panels()
+    {
+        //simply play anim for CLOSING main options panel
+        anim.Play("OptTweenAnim_off");
+        
+        //play click sfx
+        playClickSound();
+
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+    #endregion
+
+    #region Sounds
+    public void playHoverClip()
+    {
+       
+    }
+
+    void playClickSound() {
+
+    }
+
+    public void pauseGame()
+    {
+        pausePanel.SetActive(true);
+        player.SetActive(false);
+    }
+    public void resumeGame(){
+        player.SetActive(true);
+        pausePanel.SetActive(false);
+    }
+    public void homeGame()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+
+    #endregion
+}
